5ad0a8d238b99a6b372435d99d958699e536cc33
Adei Dsoo <dsoo@MacBook-Air-de-Adei.local>
2023-01-23 12:21:55 -0600

    advance in filtering y style responsive

--- proff/src/Components/StudyPages/Study.jsx
+++ proff/src/Components/StudyPages/Study.jsx
@@ -7,7 +7,7 @@ import './study.css'
 export const Study = () => {
     const [courses, setCourses] = useState([]);
     const [allCourses, setAllCourses] = useState([]);
-    const [arrayFilter, setArrayFilter]=useState([])
+    const [arrayFilter, setArrayFilter] = useState([])
 
     const getAllCourses = async () => {
         const config = {
@@ -25,7 +25,7 @@ export const Study = () => {
     }, []);
 
     const handleCourseChange = (e) => {
-        const resultSearch = allCourses.filter((item) => {
+        const resultSearch = courses.filter((item) => {
             if ((item.course).includes(e.target.value)) {
                 return item
             }
@@ -41,7 +41,7 @@ export const Study = () => {
 
         const daysCourses = [];
 
-        allCourses.forEach((element) => {
+        courses.forEach((element) => {
             const daysClass = element.availableTimes
             daysClass.forEach((day) => {
                 if (day.weekday === e.target.value) {
@@ -56,112 +56,125 @@ export const Study = () => {
     const hoursRange = (valueUser) => {
         const range = []
         const result = []
-
-        allCourses.forEach((element) => {
+       
+        courses.forEach((element) => {
             const daysClass = element.availableTimes
-            const rangeAvailableClass = daysClass.map((day) => {
+            daysClass.forEach((day) => {
                 const start = parseInt(day.startClass)
-                const end = parseInt(day.endClass) - 1
-                range.push({ element, rangeHour: [start, end] });
-                console.log(start,end,'rangeForEach')
+                const end = parseInt(day.endClass)
+                if (valueUser >= start && valueUser < end) {
+                    // console.log(start, end,'rangessss')
+                    
+                    range.push(element);
+                    console.log(element)
+                }
             });
-            return rangeAvailableClass
         });
 
-     
-        const hoursAvailable = []
-  
-        range.forEach((item) => {
-           
-            const start = item.rangeHour.shift()-1
-            const end = item.rangeHour.pop()
-      
-            let numbersHours = start;
-            for (let i = start; i < end; i++) {
-                numbersHours++
-                hoursAvailable.push(numbersHours);
-                console.log(hoursAvailable,'_____')
-            };
-            console.log({item, range: hoursAvailable},'alho')
-            // const result= hoursAvailable.filter(hour=>hour===valueUser)
-            // console.log(result,'ll')
-            // });
-// console.log(numbersHours,'numbersHours:::::::')
+        // const hoursAvailable = []
+        // console.log(range, 'range')
+        // range.forEach((item) => {
 
-            if (hoursAvailable.includes(parseInt(valueUser))) {
-                // console.log(item.element, 'gfd')
-                result.push(item.element)
-                return item.element
-            }
-        });
-        return result
+        //     const start = item.rangeHour.shift() - 1
+        //     const end = item.rangeHour.pop()
+        //     // console.log(start,end)
+        //     let numbersHours = start;
+        //     for (let i = start; i < end; i++) {
+        //         numbersHours++
+        //         hoursAvailable.push(numbersHours);
+        //         console.log(hoursAvailable, '_____')
+        //     };
+        //     console.log({ item, range: hoursAvailable }, 'alho')
+        //     // const result= hoursAvailable.filter(hour=>hour===valueUser)
+        //     // console.log(result,'ll')
+        //     // });
+        //     // console.log(numbersHours,'numbersHours:::::::')
+
+        // if (hoursAvailable.includes(parseInt(valueUser))) {
+        //     // console.log(item.element, 'gfd')
+        //     result.push(item.element)
+        //     return item.element
+        // }
+    // });
+    let results = [];
+    for (let professor of range) {
+        if (!result.includes(professor)) {
+          result.push(professor);
+        }
+      }
+    console.log(result,'resultssss')
+    console.log(range,'range')
+//    if (range.find((item) => item.id === range.id)) {
+//    console.log(item)
+// }
+    return result
+};
+
+const handleScheduleChange = (e) => {
+    if (e.target.value === '') {
+        getAllCourses()
     };
+    setCourses(hoursRange(e.target.value));
+};
 
-    const handleScheduleChange = (e) => {
-        if (e.target.value === '') {
-            getAllCourses()
-        };
-        setCourses(hoursRange(e.target.value));
-    };
-
-    return (
-        <main className='study-main'>
-            <Header />
-            <section className='welcomeStudents'>
-                <h2>Estos son los <br /> proffs disponibles.</h2>
-            </section>
-            <section className='menu-filtering'>
-                <label htmlFor="course">
-                    <span>Materia</span>
-                    <select onChange={(e) => handleCourseChange(e)} className='menu-filtering--select' id='course' required >
-                        <option value=''></option>
-                        <option value='Artes'>Artes</option>
-                        <option value='Biología'>Biología</option>
-                        <option value='Ciencias'>Ciencias</option>
-                        <option value='Educación Fisica' >Educación Fisica</option>
-                        <option value='Física'>Física</option>
-                        <option value='Geografía'>Geografía</option>
-                        <option value='Matemática'>Matemática</option>
-                        <option value='Portugués'>Portugués</option>
-                        <option value='Química'>Química</option>
-                    </select>
-                </label>
-                <label>
-                    <span>Día de la Semana</span>
-                    <select onChange={(e) => handleWeekDayChange(e)} className='menu-filtering--select' name="weekday" id="weekday" required>
-                        <option value=''></option>
-                        <option value="Lunes">Lunes</option>
-                        <option value="Martes">Martes</option>
-                        <option value="Miercoles">Miercoles</option>
-                        <option value="Jueves">Jueves</option>
-                        <option value="Viernes">Viernes</option>
-                    </select>
-                </label>
-                <label>
-                    <span>Horario</span>
-                    <select onChange={(e) => handleScheduleChange(e)} className='menu-filtering--select' name="schedule" id="schedule" required>
-                        <option value=''></option>
-                        <option value="6">6 hrs</option>
-                        <option value="7">7 hrs</option>
-                        <option value="8">8 hrs</option>
-                        <option value="9">9 hrs</option>
-                        <option value="10">10 hrs</option>
-                        <option value="11">11 hrs</option>
-                        <option value="12">12 hrs</option>
-                        <option value="13">13 hrs</option>
-                        <option value="14">14 hrs</option>
-                        <option value="15">15 hrs</option>
-                        <option value="16">16 hrs</option>
-                        <option value="17">17 hrs</option>
-                        <option value="18">18 hrs</option>
-                        <option value="19">19 hrs</option>
-                        <option value="20">20 hrs</option>
-                        <option value="21">21 hrs</option>
-                    </select>
-                </label>
-            </section>
-            <Professor courses={courses} />
-            {courses.length === 0 && <NoMatch />}
-        </main>
-    );
+return (
+    <main className='study-main'>
+        <Header />
+        <section className='welcomeStudents'>
+            <h2>Estos son los <br /> proffs disponibles.</h2>
+        </section>
+        <section className='menu-filtering'>
+            <label htmlFor="course">
+                <span>Materia</span>
+                <select onChange={(e) => handleCourseChange(e)} className='menu-filtering--select' id='course' required >
+                    <option value=''></option>
+                    <option value='Artes'>Artes</option>
+                    <option value='Biología'>Biología</option>
+                    <option value='Ciencias'>Ciencias</option>
+                    <option value='Educación Fisica' >Educación Fisica</option>
+                    <option value='Física'>Física</option>
+                    <option value='Geografía'>Geografía</option>
+                    <option value='Matemática'>Matemática</option>
+                    <option value='Portugués'>Portugués</option>
+                    <option value='Química'>Química</option>
+                </select>
+            </label>
+            <label>
+                <span>Día de la Semana</span>
+                <select onChange={(e) => handleWeekDayChange(e)} className='menu-filtering--select' name="weekday" id="weekday" required>
+                    <option value=''></option>
+                    <option value="Lunes">Lunes</option>
+                    <option value="Martes">Martes</option>
+                    <option value="Miercoles">Miercoles</option>
+                    <option value="Jueves">Jueves</option>
+                    <option value="Viernes">Viernes</option>
+                </select>
+            </label>
+            <label>
+                <span>Horario</span>
+                <select onChange={(e) => handleScheduleChange(e)} className='menu-filtering--select' name="schedule" id="schedule" required>
+                    <option value=''></option>
+                    <option value="6">6 hrs</option>
+                    <option value="7">7 hrs</option>
+                    <option value="8">8 hrs</option>
+                    <option value="9">9 hrs</option>
+                    <option value="10">10 hrs</option>
+                    <option value="11">11 hrs</option>
+                    <option value="12">12 hrs</option>
+                    <option value="13">13 hrs</option>
+                    <option value="14">14 hrs</option>
+                    <option value="15">15 hrs</option>
+                    <option value="16">16 hrs</option>
+                    <option value="17">17 hrs</option>
+                    <option value="18">18 hrs</option>
+                    <option value="19">19 hrs</option>
+                    <option value="20">20 hrs</option>
+                    <option value="21">21 hrs</option>
+                </select>
+            </label>
+        </section>
+        <Professor courses={courses} />
+        {courses.length === 0 && <NoMatch />}
+    </main>
+);
 }
